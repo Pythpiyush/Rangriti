7d52dde964f9916c482e8f3ff52944e0a4651e15
Piyush Agrawal <63116079+Pythpiyush@users.noreply.github.com>
2026-07-19 19:20:11 +0545

    Update Footer.jsx2

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ function Footer() {
       className="bg-pink-700 text-white py-10 mt-16"
     >
       <div className="max-w-7xl mx-auto px-6 text-center">
-        <h2 className="text-2xl font-bold">Maa's Boutique</h2>
+        <h2 className="text-2xl font-bold">Maa's Boutiquesss</h2>
 
         <p className="mt-3">
           📍 Visit us at our home boutique
