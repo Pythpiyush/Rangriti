ca61ff00dc4e4de168a7632913f74b30bade85c3
Piyush Agrawal <63116079+Pythpiyush@users.noreply.github.com>
2026-07-19 19:25:47 +0545

    Update NewFooter.jsx2

--- src/components/NewFooter.jsx
+++ src/components/NewFooter.jsx
@@ -1,24 +1,7 @@
 function NewFooter() {
   return (
-    <footer
-      id="contact"
-      className="bg-pink-700 text-white py-10 mt-16"
-    >
-      <div className="max-w-7xl mx-auto px-6 text-center">
-        <h2 className="text-2xl font-bold">Maa's Boutique</h2>
-
-        <p className="mt-3">
-          📍 Visit us at our home boutiquess
-        </p>
-
-        <p className="mt-2">
-          📞 WhatsApp: +91 XXXXXXXXXX
-        </p>
-
-        <p className="mt-2 text-pink-100">
-          Kurtis • Sarees • Bedsheets
-        </p>
-      </div>
+    <footer className="bg-black text-white p-20 text-center text-5xl">
+      THIS IS THE NEW FOOTER
     </footer>
   );
 }
